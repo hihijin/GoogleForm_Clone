568817782a877ea7298d77e659cc50b488b39e19
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-20 19:29:57 +0900

    [Feat] : 라우터, 지연로딩 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import './App.css';
+import './Global.css';
 
-import React from 'react';
+import React, {
+	lazy,
+	Suspense,
+} from 'react';
+
+import {
+	Route,
+	Routes,
+} from 'react-router-dom';
+
+import Loading from './pages/Loading';
+
+const MainPage = lazy(() => import('./pages/Mainpage'));
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route path="/" element={<MainPage />} />
+				</Routes>
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
