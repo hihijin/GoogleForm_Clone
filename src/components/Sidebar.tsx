4274c2cf87b55329024705cd63075e8c44d5ff65
hihijin <117073214+hihijin@users.noreply.github.com>
2023-06-24 18:05:38 +0900

    [Feat] : 사이드바 질문 추가버튼 구현

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,9 +2,14 @@ import '../Global.css';
 
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+
+import { ADD } from '../reducer/QuestionReducer';
+import { RootState } from '../store/Store';
+import { Iquestion, Iquestions } from '../type/Iquestion';
 
 const Main = styled.div`
 	display: flex;
@@ -16,15 +21,33 @@ const Main = styled.div`
 	border-radius: 8px;
 	padding: 10px;
 	box-shadow: 0px 1px 2px 0 rgba(0, 0, 0, 0.4);
+	&:hover {
+		cursor: pointer;
+		background: rgba(255, 255, 255, 0.5);
+	}
 	.color {
 		color: rgba(0, 0, 0, 0.6);
 	}
 `;
 
 function Sidebar() {
+	const dispatch = useDispatch();
+	const nowQuestion = useSelector(
+		(state: RootState) => state.nowQuestion,
+	) as Iquestion;
+
+	const questions = useSelector(
+		(state: RootState) => state.question,
+	) as Iquestions;
+
+	const addHandler = () => {
+		console.log('추가');
+		//현재 질문을 질문목록에 추가
+		dispatch(ADD({ ...nowQuestion, id: questions.length + 1 }));
+	};
 	return (
 		<Main>
-			<AddCircleOutlineOutlinedIcon className="color" />
+			<AddCircleOutlineOutlinedIcon onClick={addHandler} className="color" />
 		</Main>
 	);
 }
